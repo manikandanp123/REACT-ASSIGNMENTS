d9b40d125020b018c68a7fcbfcf9028727f3e82f
manikandanp123 <pmanimanu88@gmail.com>
2022-02-06 20:40:55 +0530

    done 2nd assignment of react

--- Assignment-2/code/my-app/src/App.js
+++ Assignment-2/code/my-app/src/App.js
@@ -1,14 +1,25 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 
 
 function App(props) {
  const {employee}=props;
-    
+    // console.log(employee);
 return(
   <div className="container">
-  {/* Start your React code here */}
+
+    <div><img src={employee.profileImg} alt="photo" /></div>
+    <div className="inner">
+    <h1>{employee.name}</h1>
+    <p>Location</p>
+    <p className='bright'>{employee.location}</p>
+    <p>Blood Group</p>
+    <p className='bright'>{employee.bloodGroup}</p>
+    <p>Age</p>
+    <p className='bright'>{employee.age}</p>
+    </div>
+
 </div>
 )
 }
